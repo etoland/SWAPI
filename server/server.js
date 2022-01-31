9b4d024d90d297f96bef93a76783c26269fa959f
“Emma <etoland007@gmail.com>
2022-01-31 11:05:34 -0800

    110%

--- server/server.js
+++ server/server.js
@@ -41,11 +41,11 @@ app.get("/starships", async (req, res) => {
 });
 
 app.get("/people/:id/", async (req, res) => {
-  const api_url = "https://swapi.dev/api/people";
+  const { id } = req.params;
+  const api_url = `https://swapi.dev/api/people/${id}`;
   const fetch_response = await fetch(api_url);
   const json = await fetch_response.json();
-  console.log(json);
-  res.status(200).json({ json });
+  res.json(json);
 });
 
 app.listen(PORT, function () {
